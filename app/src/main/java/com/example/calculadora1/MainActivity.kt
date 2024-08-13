a7b616db89493d413d62f425d81138e73e8cc38c
Rajold <nmphservices@gmail.com>
2024-08-12 22:50:06 -0500

    vibrateFunction

--- app/src/main/java/com/example/calculadora1/MainActivity.kt
+++ app/src/main/java/com/example/calculadora1/MainActivity.kt
@@ -28,6 +28,8 @@ vibrator= getSystemService(VIBRATOR_SERVICE) as Vibrator?
       var fontSizes= 60f
       var warningSizes= 40f
       val calculo= binding.tvinput
+      val fNumber= 0
+      val sNumber= 0
 
       fun fontSizeDown(){
         if (binding.tvinput.text.length > 8
@@ -53,83 +55,103 @@ vibrator= getSystemService(VIBRATOR_SERVICE) as Vibrator?
       }
 
       binding.punt.setOnClickListener {
-
+          vibrator?.vibrate(30)
           calculo.text = "${calculo.text}."
       }
       binding.cero.setOnClickListener {
         calculo.text= "${calculo.text}0"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.uno.setOnClickListener {
             calculo.text= "${calculo.text}1"
         fontSizeDown()
+          vibrator?.vibrate(30)
         }
       binding.dos.setOnClickListener {
         calculo.text= "${calculo.text}2"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.tres.setOnClickListener {
         calculo.text= "${calculo.text}3"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.cuat.setOnClickListener {
         calculo.text= "${calculo.text}4"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.cinc.setOnClickListener {
         calculo.text= "${calculo.text}5"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.seis.setOnClickListener {
         calculo.text= "${calculo.text}6"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.siet.setOnClickListener {
         calculo.text= "${calculo.text}7"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.ocho.setOnClickListener {
         calculo.text= "${calculo.text}8"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
       binding.nuev.setOnClickListener {
         calculo.text= "${calculo.text}9"
         fontSizeDown()
+          vibrator?.vibrate(30)
       }
 
       binding.perc.setOnClickListener {
         calculo.text= "${calculo.text}%"
+          vibrator?.vibrate(30)
       }
       binding.raiz.setOnClickListener {
         calculo.text= "${calculo.text}√"
+          vibrator?.vibrate(30)
       }
       binding.div.setOnClickListener {
         calculo.text= "${calculo.text}/"
+          vibrator?.vibrate(30)
       }
       binding.mult.setOnClickListener {
         calculo.text= "${calculo.text}*"
+          vibrator?.vibrate(30)
       }
       binding.rest.setOnClickListener {
         calculo.text= "${calculo.text}-"
+          vibrator?.vibrate(30)
       }
       binding.suma.setOnClickListener {
         calculo.text= "${calculo.text}+"
+          vibrator?.vibrate(30)
       }
 
       binding.pi.setOnClickListener {
         calculo.text= "${calculo.text}π"
+          vibrator?.vibrate(30)
       }
 
       binding.para.setOnClickListener {
         calculo.text= "${calculo.text}("
+          vibrator?.vibrate(30)
       }
 
       binding.parc.setOnClickListener {
         calculo.text= "${calculo.text})"
+          vibrator?.vibrate(30)
       }
 
       binding.sign.setOnClickListener {
         calculo.text= "${calculo.text}*-1"
+          vibrator?.vibrate(30)
       }
 
 
@@ -137,12 +159,14 @@ vibrator= getSystemService(VIBRATOR_SERVICE) as Vibrator?
         calculo.text= calculo.text.dropLast(1)
         binding.tvOutput.text= ""
         fontSizeUp()
+          vibrator?.vibrate(30)
       }
       binding.ce.setOnClickListener {
         calculo.text= ""
         binding.tvOutput.text= ""
             fontSizes =60f
             binding.tvinput.setTextSize(TypedValue.COMPLEX_UNIT_DIP, fontSizes)
+          vibrator?.vibrate(30)
       }
 
       binding.igua.setOnClickListener {
@@ -153,10 +177,12 @@ vibrator= getSystemService(VIBRATOR_SERVICE) as Vibrator?
           warningSizes =20f
           binding.tvOutput.setTextSize(TypedValue.COMPLEX_UNIT_DIP, warningSizes)
           binding.tvOutput.setTextColor(Color.parseColor("#000000"))
+            vibrator?.vibrate(30)
         }
         else{
           binding.tvinput.text= resultadoCalc.toString()
           fontSizeDown()
+            vibrator?.vibrate(30)
         }
       }
 
